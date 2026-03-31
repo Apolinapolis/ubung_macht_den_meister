864832de8602e52300962e609b6d73cc33e5179e
Apolinapolis <larra12@ya.ru>
2026-03-31 12:37:14 +0300

    new iteration after Oz

--- boom.py
+++ boom.py
@@ -1,6 +1,4 @@
 # Есть JSON, нужно вытащить из него уникальные цвета (color) для аксессуаров типа phoneCase:
-from fontTools.t1Lib import stringToLong
-from numpy.ma.core import append
 
 json_data = {
     "phone": {
@@ -20,8 +18,7 @@ json_data = {
 uniq_colors = {acc['color'] for acc in json_data['phone']['accessories'] if acc['type'] == 'phoneCase'}
 
 # найти наибольший int
-
-s = 'i8sdgf928ury63jds9sf9a3333'
+s = 'i8sdgf928ury63jds9sf9a333'
 
 def func(s):
     result = []
@@ -40,54 +37,70 @@ def func(s):
 
 print(func(s))
 
-# TODO  = 01 __eq__ / 02 review / 06 what / 07 csv
+# TODO
+
+# Страница https://company.local/api/v1/devices возвращает список устройств в JSON.
+# Задача — получать ежеминутно список и записывать в CSV (device_name, interface_name).
+# [{  "id": 1,
+#     "name": "Device1",
+#     "interfaces": [
+#       {"id": 101, "name": "Ethernet0"},
+#       {"id": 102, "name": "Ethernet1"}]}]
+import csv
+import time
+import requests
+
+URL = 'https://company.local/api/v1/devices'
+
+def get_devices():
+    response = requests.get(URL)
+    response.raise_for_status()
+    return response.json()
+
+
+def parse_devices(devices_data):
+    result = []
+    for el in devices_data:
+        dev_name = el.get('name')
+        for interface in el.get('interfaces', []):
+            interface_name = interface.get('name')
+            result.append({'device_name':dev_name, 'interface_name':interface_name})
+    return result
+
+
+def save_to_csv(data):
+    with open('devices.csv', 'w') as file:
+        writer = csv.DictWriter(file, fieldnames=['device_name','interface_name'])
+        writer.writeheader()
+        writer.writerows(data)
+
+
+def devices_loop():
+    while True:
+        devices = get_devices()
+        parsed_devices = parse_devices(devices)
+        save_to_csv(parsed_devices)
+        time.sleep(60)
+
 
 # # Вернуть сумму вхождений
 # statuses = 'skip, pass, failed, failed, pass, pass, error, skip, error, error'
 #
-# def summarize_result(data:str)->dict:
-#     data = data.split(',')
-#     counter = {}
-#     for el in data:
-#         el = el.strip()
-#         if el in counter:
-#             counter[el]+=1
-#         else:
-#             counter[el] = 1
-#     return dict(sorted(counter.items(), key=lambda x: x[1], reverse=True)) # Добавлена сортировка
+def summarize_result(data:str)->dict:
+    data = data.split(',')
+    counter = {}
+    for el in data:
+        el = el.strip()
+        if el in counter:
+            counter[el]+=1
+        else:
+            counter[el] = 1
+    return dict(sorted(counter.items(), key=lambda x: x[1], reverse=True)) # Добавлена сортировка
 #
 #
 # # Вариант через импорт
 # def summarize_result_two(data:str)->dict:
 #     return dict(Counter(x.strip() for x in data.split(',')))
-
-
-# Написать класс Car:  свойства color (текст),  price (нецелое число). Метод get_final_price — если цвет «красный», цена на 15% дороже от базовой.
-# Создать класс HeavyCar, унаследованный от Car: свойство has_trailer (булево). Переопределить get_final_price: прицеп — +25% от базовой цены. Использовать super().
-
-# class Car:
-#     def __init__(self, color:str, price:float):
-#         self.color = color
-#         self.price = price
-#
-#     def get_final_price(self):
-#         if self.color == 'red':
-#             return self.price * 1.15
-#         return self.price
-
-
-# class HeavyCar(Car):
-#     def __init__(self, color:str, price:float, has_trailer:bool):
-#         self.has_trailer = has_trailer
-#         super().__init__(color, price)
-#
-#     def get_final_price(self, digits=2)->float:
-#         price = super().get_final_price()
-#
-#         if self.has_trailer:
-#             price += self.price * 0.25
-#
-#         return round(price, digits)
 
 
 # Фикстуры и параметризация
@@ -97,26 +110,21 @@ print(func(s))
 # 3) Написать тест с фикстурой из 1: проверить, что есть пользователи из Москвы;
 # вернуть их количество в фикстуру и вывести в консоль через print().
 
-import pytest
-
-@pytest.fixture
-def users():
-    return  [{'name': 'Nikolay', 'last_name': 'Petrov', 'age': 43, 'city': 'Moscow'},
-    {'name': 'Ivan', 'age': 43, 'last_name': 'Volkov', 'city': 'Rostov'},
-    {'name': 'Sergey', 'age': 23, 'last_name': 'Pak', 'city': 'Yakutsk'},
-    {'name': 'Ivan', 'age': 19, 'last_name': 'Ivanov', 'city': 'Tver'},
-    {'name': 'Nikolay', 'age': 41, 'last_name': 'Ivanov', 'city': 'Moscow'}]
-
-def test_no_less_18(users):
-    for user in users:
-        assert user['age'] >= 18
-
-
 users_data = [{'name': 'Nikolay', 'last_name': 'Petrov', 'age': 43, 'city': 'Moscow'},
     {'name': 'Ivan', 'age': 43, 'last_name': 'Volkov', 'city': 'Rostov'},
     {'name': 'Sergey', 'age': 23, 'last_name': 'Pak', 'city': 'Yakutsk'},
     {'name': 'Ivan', 'age': 19, 'last_name': 'Ivanov', 'city': 'Tver'},
     {'name': 'Nikolay', 'age': 41, 'last_name': 'Ivanov', 'city': 'Moscow'}]
+
+import pytest
+
+@pytest.fixture
+def users():
+    return users_data
+
+def test_no_less_18(users):
+    for user in users:
+        assert user['age'] >= 18
 
 @pytest.mark.parametrize('user', users_data)
 def test_less_60_users_age(user):
@@ -133,30 +141,30 @@ def test_moscow_users(msk_users_count):
 
 
 # Чтобы fixture вызывалась в теле теста и возвращала список random int по длине count а после теста очищаем список
-# import random
-#
-# def generate_item()->int:
-#     return random.randint(1,20)
-#
-# @pytest.fixture
-# def setup_items():
-#     def _gen(count:int):
-#         return [generate_item() for _ in range(count)]
-#     return _gen
-#
-# @pytest.fixture
-# def setup_items_clear():
-#     items = []
-#     def _gen(count:int):
-#         items.clear() #fix
-#         for _ in range(count):
-#             items.append(generate_item())
-#         return items
-#     yield _gen
-#     items.clear()
-#
-# def op_test_a(setup_items):
-#     item_ids = setup_items(count=3)
+import random
+
+def generate_item()->int:
+    return random.randint(1,20)
+
+@pytest.fixture
+def setup_items():
+    def _gen(count:int):
+        return [generate_item() for _ in range(count)]
+    return _gen
+
+@pytest.fixture
+def setup_items_clear():
+    items = []
+    def _gen(count:int):
+        items.clear() #fix
+        for _ in range(count):
+            items.append(generate_item())
+        return items
+    yield _gen
+    items.clear()
+
+def op_test_a(setup_items):
+    item_ids = setup_items(count=3)
 
 
 # Уникальные символы = (
@@ -207,30 +215,60 @@ def test_moscow_users(msk_users_count):
 # print(incoming_counter(statuses))
 
 
-# Написать класс Car:  свойства color (текст),  price (нецелое число). Метод get_final_price — если цвет «красный», цена на 15% дороже от базовой.
+# Написать класс Car:  свойства color (текст),  price (нецелое число).
+# Метод get_final_price — если цвет «красный», цена на 15% дороже от базовой.
 # Создать класс HeavyCar, унаследованный от Car: свойство has_trailer (булево).
-# Переопределить get_final_price: прицеп — +25% от базовой цены. Использовать super(). Итог округлить до 2х цифр после запятой
+# Переопределить get_final_price: прицеп — +25% от базовой цены. Использовать super().
+# Итог округлить до 2х цифр после запятой
 
-# class Car:
-#     def __init__(self, color, price):
-#         self.color = color
-#         self.price = price
+class Car:
+    def __init__(self, color, price):
+        self.color = color
+        self.price = price
+
+    def get_final_price(self):
+        if self.color == 'red':
+            return self.price * 1.15
+        return self.price
+
+
+class HeavyCar(Car):
+    def __init__(self, color, price, has_trailer):
+        super().__init__(color, price)
+        self.has_trailer = has_trailer
+
+    def get_final_price(self, digits=2):
+        final_price = super().get_final_price()
+        if self.has_trailer:
+            final_price += self.price * 0.25
+        return round(final_price, digits)
+
+
+
+# Что будет выведено? Если тест падает — как сделать, чтобы проходил?
+
+class User:
+    height = None
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __eq__(self, other):
+        if isinstance(other, User):
+            return self.name == other.name and self.age == other.age
+        return NotImplemented
+
+    def __hash__(self):
+        return hash((self.name, self.age))
 #
-#     def get_final_price(self):
-#         if self.color == 'red':
-#             return self.price * 1.15
-#         return self.price
-#
-#
-# class HeavyCar(Car):
-#     def __init__(self, color, price, has_trailer):
-#         super().__init__(color, price)
-#         self.has_trailer = has_trailer
-#
-#     def get_final_price(self, digits=2):
-#         final_price = super().get_final_price()
-#         if self.has_trailer:
-#             final_price += self.price * 0.25
-#         return round(final_price, digits)
-#
-# print(HeavyCar('red',100,True).get_final_price(0))
+# def main():
+#     user_set = set()
+#     user_set.add(User("Max", 12))
+#     user_set.add(User("Max", 12))
+#     user_set.add(User("Max", 12))
+#     user_set.add(User("Max", 12))
+#     if len(user_set) == 1:
+#         print("Passed!")
+#     else:
+#         print("Failed!")
--- kata.py
+++ kata.py
@@ -1,5 +1,5 @@
 class File:
-    """Итератор"""
+    """CONTEXT"""
     def __init__(self,path, mode='r'):
         self.path = path
         self.mode = mode
@@ -40,3 +40,72 @@ class Child(Example):
     'Dockstring only'
 
 new = Child()
+
+
+#IT_one задача на sql
+
+# /*CREATE TABLE customers (
+#  id INTEGER NOT NULL PRIMARY KEY,
+#  name VARCHAR(30) NOT NULL,
+#  cardNum INTEGER
+# );*/
+#
+# CREATE TABLE accounts (
+#  id INTEGER NOT NULL PRIMARY KEY,
+#  customer_id INTEGER NOT NULL,
+#  balance INTEGER,
+#  payment_system VARCHAR(30) NOT NULL,
+#  FOREIGN KEY (customer_id) REFERENCES customers(id)
+# );
+#
+# INSERT INTO customers(id, name, cardNum) values(1, 'Иван Грозный', 4532);
+# INSERT INTO customers(id, name, cardNum) values(2, 'Екатерина Романова', 7764);
+# INSERT INTO customers(id, name, cardNum) values(3, 'Николай Романов', 2323);
+# INSERT INTO customers(id, name, cardNum) values(4, 'Владимир Ленин', 9970);
+# INSERT INTO customers(id, name, cardNum) values(5, 'Леонид Брежнев', 1231);
+# INSERT INTO customers(id, name, cardNum) values(6, 'Борис Ельцин', NULL);
+#
+# INSERT INTO accounts(id, customer_id, balance, payment_system) values(1, 1, 123.00, 'VISA');
+# INSERT INTO accounts(id, customer_id, balance, payment_system) values(2, 2, 555.00, 'VISA');
+# INSERT INTO accounts(id, customer_id, balance, payment_system) values(3, 4, 222.00, 'MasterCard');
+# INSERT INTO accounts(id, customer_id, balance, payment_system) values(4, 3, 333.00, 'MasterCard');
+# INSERT INTO accounts(id, customer_id, balance, payment_system) values(5, 5, 444.00, 'UnionPay');
+# INSERT INTO accounts(id, customer_id, balance, payment_system) values(6, 6, 0.00, 'UnionPay');
+#
+# 1. Вывести название и максимальный баланс для каждой платежной системы.
+# 2. Вывести имя клиента, номер карты, баланс, платежную систему.
+# 3. Только клиентов с платежной системой VISA, отсортировать по балансу по убыванию.
+
+
+#Задачи на Python
+
+# Описание:
+# Требуется реализовать вывод функции print 2х классов: Parent, Child.
+# При двух ограничениях: 1 - нельзя писать вывод Parent класса прямым текстом. 2 - нельзя менять наследуемость класса Child
+# Можно менять поведение класса Child
+# Результат вывести на экран.
+
+# Входные данные:
+# class Parent:
+#     def __init__(self):
+#         print("Parent")
+#
+# class Child(Parent):
+#     def __init__(self):
+#         print("Child")
+
+# Результат:
+# Parent
+# Child
+
+
+# Описание:
+# Требуется реализовать функцию, которая будет искать повторяющиеся числа в списке.
+# Числа, значению записаны в виде list.
+# Результат вывести на экран.
+
+# Входные данные:
+# [1, 2, 3, 2, 4, 1, 5, 2]
+
+# Результат:
+# (1, 2)
